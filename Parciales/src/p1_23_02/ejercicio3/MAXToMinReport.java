8bf1fe6d07ce0d209a3b7b522eba5f6e2e746f66
Mati Sanchez <msancheznovelli@itba.edu.ar>
2025-04-24 12:29:06 -0300

    Practica primer parcial

---
+++ Parciales/src/p1_23_02/ejercicio3/MAXToMinReport.java
@@ -0,0 +1,15 @@
+package p1_23_02.ejercicio3;
+
+import java.util.Comparator;
+
+public class MAXToMinReport<T> extends MINToMaxReport<T> {
+
+    public MAXToMinReport(Comparator<? super T> cmp) {
+        super(new Comparator<T>() {
+            @Override
+            public int compare(T o1, T o2) {
+                return cmp.compare(o2, o1);
+            }
+        });
+    }
+}
